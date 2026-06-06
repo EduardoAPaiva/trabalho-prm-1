f2c47c377fbf8122896250d22dda76d45b99d944
EduardoAPaiva <edalpa0@gmail.com>
2026-06-06 14:07:23 -0300

    Melhorando POSICIONANDO_NA_BANDEIRA

--- pega_bandeiras/pega_bandeiras/controle_robo.py
+++ pega_bandeiras/pega_bandeiras/controle_robo.py
@@ -53,10 +53,14 @@ class ControleRobo(Node):
         self.chegou_na_bandeira = False
         self.giro_desvio = 0
 
+        self.const_dist_obstaculo = 0.7
+
         self.pos_x_bandeira = -1
+        self.pos_x_mastro = -1
         self.ultima_posx_bandeira = -1
         self.centro_x = -1
-        self.dx = 30
+        self.dx = 20
+        self.dx_mastro = 5
         self.distancia_frente = float('inf')
 
     def scan_callback(self, msg: LaserScan):
@@ -91,13 +95,13 @@ class ControleRobo(Node):
 
         self.distancia_frente = msg.ranges[0]
         
-        if distancias_frente_esquerda and min(distancias_frente_esquerda) < 0.7:
+        if distancias_frente_esquerda and min(distancias_frente_esquerda) < self.const_dist_obstaculo:
             self.obstaculo_a_frente = True
             self.direcao_obstaculo = -1
-        elif distancias_frente_direita and min(distancias_frente_direita) < 0.7:
+        elif distancias_frente_direita and min(distancias_frente_direita) < self.const_dist_obstaculo:
             self.obstaculo_a_frente = True
             self.direcao_obstaculo = 1
-        elif distancias_frente_direita and min(distancias_frente_direita) < 0.7 and distancias_frente_esquerda and min(distancias_frente_esquerda) < 0.7:
+        elif distancias_frente_direita and min(distancias_frente_direita) < self.const_dist_obstaculo and distancias_frente_esquerda and min(distancias_frente_esquerda) < self.const_dist_obstaculo:
             if min(distancias_frente_esquerda) < min(distancias_frente_direita):
                 self.direcao_obstaculo = -1
             else:
@@ -165,8 +169,45 @@ class ControleRobo(Node):
 
             # Ajuste esse limiar experimentalmente
             self.chegou_na_bandeira = self.percentual_bandeira > 0.025
-
         
+        if self.estado_atual == ESTADOS.POSICIONANDO_NA_BANDEIRA:
+            # Define limite inferior (apenas a parte inferior da imagem)
+            y_limite = int(h * 2/3)
+
+            # Cria uma máscara apenas da região inferior
+            mask_inferior = np.zeros_like(mask)
+            mask_inferior[y_limite:h, :] = mask[y_limite:h, :]
+
+            # Quantos pixels da imagem pertencem à bandeira na região inferior
+            pixels_bandeira = cv2.countNonZero(mask_inferior)
+            area_imagem_inferior = (h - y_limite) * w
+            self.percentual_bandeira = pixels_bandeira / area_imagem_inferior
+
+            # Detecta contornos apenas na região inferior
+            contours, _ = cv2.findContours(
+                mask_inferior,
+                cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE
+            )
+
+            if contours:
+                # Seleciona apenas o maior blob
+                maior_contorno = max(contours, key=cv2.contourArea)
+
+                # Metade da altura da imagem
+                y_meio = h // 2
+
+                # Mantém apenas os pontos do contorno na metade inferior
+                pontos_inferiores = maior_contorno[maior_contorno[:, 0, 1] >= y_meio]
+
+                if len(pontos_inferiores) > 2:  # necessário para formar um contorno
+                    M = cv2.moments(pontos_inferiores)
+
+                    if M['m00'] != 0:
+                        cx = int(M['m10'] / M['m00'])
+                        cy = int(M['m01'] / M['m00'])
+
+                        self.pos_x_mastro = cx
 
     def move_robot(self):
 
@@ -181,7 +222,7 @@ class ControleRobo(Node):
         if self.chegou_na_bandeira and self.obstaculo_a_frente:
             self.estado_atual = ESTADOS.POSICIONANDO_NA_BANDEIRA
 
-        elif self.estado_atual == ESTADOS.EXPLORANDO:
+        if self.estado_atual == ESTADOS.EXPLORANDO:
             if not self.obstaculo_a_frente:
                 twist.linear.x = 0.5  # Move para frente
             else:
@@ -210,12 +251,13 @@ class ControleRobo(Node):
             print(self.percentual_bandeira)
 
             if not self.obstaculo_a_frente:
-                if self.bandeira_a_frente and not (self.pos_x_bandeira < self.centro_x + self.dx and self.pos_x_bandeira > self.centro_x - self.dx):
+                if self.pos_x_bandeira != None and not (self.pos_x_bandeira < self.centro_x + self.dx and self.pos_x_bandeira > self.centro_x - self.dx):
                     self.estado_atual = ESTADOS.BANDEIRA_ENCONTRADA
                     self.giro_desvio = 0
                 if self.giro_desvio != 0:
                     twist.angular.z = self.giro_desvio * 0.3
                 else:
+                    self.giro_desvio = 0
                     twist.linear.x = 0.5
             else:
                 self.estado_atual = ESTADOS.DESVIANDO 
@@ -226,7 +268,7 @@ class ControleRobo(Node):
         elif self.estado_atual == ESTADOS.DESVIANDO:
             if not self.obstaculo_a_frente:
                 if self.direcao_obstaculo == -1:
-                    if self.pos_x_bandeira > self.centro_x + self.dx :
+                    if self.pos_x_bandeira != None and self.pos_x_bandeira > self.centro_x + self.dx :
                         self.estado_atual = ESTADOS.INDO_PARA_BANDEIRA
                     elif self.menor_distancia_esquerda < 90:
                         twist.linear.x = 0.5
@@ -236,7 +278,7 @@ class ControleRobo(Node):
                         twist.angular.z = 0.3
                         self.estado_atual = ESTADOS.INDO_PARA_BANDEIRA
                 elif self.direcao_obstaculo == 1:
-                    if self.pos_x_bandeira < self.centro_x - self.dx :
+                    if self.pos_x_bandeira != None and self.pos_x_bandeira < self.centro_x - self.dx :
                         self.estado_atual = ESTADOS.INDO_PARA_BANDEIRA
                     elif self.menor_distancia_direita > 270:
                         twist.linear.x = 0.5
@@ -252,13 +294,15 @@ class ControleRobo(Node):
                     twist.angular.z = 0.3
 
         elif self.estado_atual == ESTADOS.POSICIONANDO_NA_BANDEIRA:
-            if self.pos_x_bandeira < self.centro_x - self.dx:
+            print(self.pos_x_bandeira, self.pos_x_mastro)
+            if self.pos_x_mastro < self.centro_x - self.dx_mastro:
                 twist.angular.z = 0.3  # Gira em torno do proprio eixo
-            elif self.pos_x_bandeira > self.centro_x + self.dx:
+            elif self.pos_x_mastro > self.centro_x + self.dx_mastro:
                 twist.angular.z = -0.3  # Gira em torno do proprio eixo
-            twist.linear.x = 0.0
-            twist.linear.y = 0.0
-            twist.angular.z = 0.0
+            else:
+                twist.linear.x = 0.0
+                twist.linear.y = 0.0
+                twist.angular.z = 0.0
 
 
         self.cmd_vel_pub.publish(twist)
